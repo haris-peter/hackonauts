87370934f891bbf2dfe8a1abe101783b6fc07ba7
KyawThu082 <kyawthu082@gmail.com>
2023-04-13 14:13:20 +0630

    complete commit

---
+++ lib/Services/global_variables.dart
@@ -0,0 +1,8 @@
+
+String loginUrlImage = 'https://images.unsplash.com/photo-1566738826204-9281873a794e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
+String signUpUrlImage = 'https://images.unsplash.com/photo-1462899006636-339e08d1844e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
+String forgeUrlImage = 'https://images.pexels.com/photos/5496469/pexels-photo-5496469.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+String? name = '';
+String? userImage = '';
+String? location = '';
+
